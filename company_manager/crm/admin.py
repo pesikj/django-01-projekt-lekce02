2442429fc177ff0fb1287fe0c647d7f69da7752f
Jiří Pešík <jiri.pesik@outlook.cz>
2022-04-03 22:10:20 +0200

    Initial commit

---
+++ company_manager/crm/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+import crm.models
+
+admin.site.register(crm.models.Company)
---
+++ company_manager/crm/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import CreateView, ListView, TemplateView
+import crm.models as models
+from django.urls import reverse_lazy
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class CompanyCreateView(CreateView):
+    model = models.Company
+    template_name = "company/create_company.html"
+    fields = ["name", "status", "phone_number", "email", "identification_number"]
+    success_url = reverse_lazy("index")
+
+class CompanyListView(ListView):
+    model = models.Company
+    template_name = "company/list_company.html"
